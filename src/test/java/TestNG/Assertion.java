230e5a1710864355181a58d6b4bb9f30d28fdf5e
sumayyabegum <sumayyab97@gmail.com>
2024-02-09 22:06:16 +0530

    selenium program

--- src/test/java/TestNG/Assertion.java
+++ src/test/java/TestNG/Assertion.java
@@ -1,5 +1,16 @@
 package TestNG;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
 public class Assertion {
+    WebDriver driver;
+    @BeforeClass
+    void setup() {
+        System.setProperty("WebDriver.Chrome.driver", "\"C:\\Program Files\\Google\\Chrome\"");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/search?q=myntra&oq=mintr&gs_lcrp=EgZjaHJvbWUqDwgBEAAYChiDARixAxiABDIGCAAQRRg5Mg8IARAAGAoYgwEYsQMYgAQyDwgCEAAYChiDARixAxiABDIMCAMQABgKGLEDGIAEMgcIBBAAGIAEMgwIBRAAGAoYsQMYgAQyEggGEAAYChiDARixAxiABBiKBTIGCAcQBRhA0gEINTEwM2owajeoAgCwAgA&sourceid=chrome&ie=UTF-8");}
+
     
 }
